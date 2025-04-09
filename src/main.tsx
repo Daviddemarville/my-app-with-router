cebf745f87db82aff84166b676c389387e5aa558
Daviddemarville <de.marville.david@gmail.com>
2025-04-09 09:55:21 +0200

    add commit Outlet

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
-import App from './App.tsx'
+import App from "./App";
 
-createRoot(document.getElementById("root")).render(<App />);
+// page components
 
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+// router creation
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+// rendering
+
+const rootElement = document.getElementById("root");
+
+if (rootElement != null) {
+  ReactDOM.createRoot(rootElement).render(
+    <RouterProvider router={router} />
+  );
+}
